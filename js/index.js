c9ae96581c5c11221daaeb65453d62d0f9c0856e
boahs <39522778+boahs@users.noreply.github.com>
2020-08-04 16:28:30 -0400

    third event listener added. load()

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
-//one wheel event on .img-content image
+//one wheel event on .img-content image [Bubbles = true]
 
 const imgWheel = document.querySelector(".img-content img");
 
 let scale = 1;
 
 function zoom(event) {
-  event.preventDefault();
+  event.preventDefault(); // preventing default action of event. In our case our image is having it's default behavior stripped.
 
-  scale += event.deltaY * -0.01;
+  scale += event.deltaY * -0.01; // we're using our scale variable with a value of 1 to + or = to our events location on the screen times our negative number
 
   // Restrict scale
-  scale = Math.min(Math.max(1, scale), 2);
+  scale = Math.min(Math.max(1, scale), 2); // now scale must equal the mininum and maximum of how big we wanna increase/decrease it
 
   // Apply scale transform
-  imgWheel.style.transform = `scale(${scale})`;
+  imgWheel.style.transform = `scale(${scale})`; // transforming our image equaling a string literal
 }
 
 imgWheel.addEventListener("wheel", zoom);
 
-//two mouseover event on header 2
+//two mouseover event on header 2 [Bubbles = true]
 
 const changeHeadColor = document.querySelector("h2");
 
@@ -26,11 +26,11 @@ changeHeadColor.addEventListener(
   "mouseenter",
   function (event) {
     // highlight the mouseenter target
-    event.target.style.color = "green";
+    event.target.style.color = "green"; // changes the style when the event is triggered
 
     // reset the color after a short delay
     setTimeout(function () {
-      event.target.style.color = "";
+      event.target.style.color = ""; // using a closure we can reset our color after 500 miliseeconds
     }, 500);
   },
   false
@@ -49,3 +49,10 @@ changeHeadColor.addEventListener(
   },
   false
 );
+
+// load event [bubbles = FALSE]
+
+window.addEventListener("load", (event) => {
+  //when we load our page we'll log our console with the following message below.
+  console.log(`We've loaded the page successfully`);
+});
