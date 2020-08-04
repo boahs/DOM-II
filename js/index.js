609ac02be3246f3c5d03e6f955db3fbfc3f00ab6
boahs <39522778+boahs@users.noreply.github.com>
2020-08-04 15:29:30 -0400

    second event listener added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//one wheel event on .img-content image
+
 const imgWheel = document.querySelector(".img-content img");
 
 let scale = 1;
@@ -8,10 +10,42 @@ function zoom(event) {
   scale += event.deltaY * -0.01;
 
   // Restrict scale
-  scale = Math.min(Math.max(0.125, scale), 4);
+  scale = Math.min(Math.max(1, scale), 2);
 
   // Apply scale transform
   imgWheel.style.transform = `scale(${scale})`;
 }
 
 imgWheel.addEventListener("wheel", zoom);
+
+//two mouseover event on header 2
+
+const changeHeadColor = document.querySelector("h2");
+
+changeHeadColor.addEventListener(
+  "mouseenter",
+  function (event) {
+    // highlight the mouseenter target
+    event.target.style.color = "green";
+
+    // reset the color after a short delay
+    setTimeout(function () {
+      event.target.style.color = "";
+    }, 500);
+  },
+  false
+);
+
+changeHeadColor.addEventListener(
+  "mouseover",
+  function (event) {
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+
+    // reset the color after a short delay
+    setTimeout(function () {
+      event.target.style.color = "";
+    }, 500);
+  },
+  false
+);
