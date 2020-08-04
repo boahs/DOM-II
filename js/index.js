8e909f78db18da7ad155813c29bd4dcc8d99c178
boahs <39522778+boahs@users.noreply.github.com>
2020-08-04 15:21:26 -0400

    one added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+const imgWheel = document.querySelector(".img-content img");
+
+let scale = 1;
+
+function zoom(event) {
+  event.preventDefault();
+
+  scale += event.deltaY * -0.01;
+
+  // Restrict scale
+  scale = Math.min(Math.max(0.125, scale), 4);
+
+  // Apply scale transform
+  imgWheel.style.transform = `scale(${scale})`;
+}
+
+imgWheel.addEventListener("wheel", zoom);
